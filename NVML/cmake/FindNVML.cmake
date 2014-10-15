a2d5bf08a2a5221b9827eaeffccd47790590a23b
Jiri Kraus <jkraus@nvidia.com>
2014-10-15 02:28:04 -0700

    Updated FindNVML module according to James Feedback.

--- NVML/cmake/FindNVML.cmake
+++ NVML/cmake/FindNVML.cmake
@@ -2,19 +2,18 @@
 # FindNVML
 # --------
 #
-# Find NVML
+# Find the NVIDIA Management Library (NVML) includes and library. NVML documentation
+# is available at: http://docs.nvidia.com/deploy/nvml-api/index.html 
 #
-# Find the NVIDIA Management Library (NVML) includes and library.
-# Documentation is available at: http://docs.nvidia.com/deploy/nvml-api/index.html 
+# NVML is part of the GPU Deployment Kit (GDK) and GPU_DEPLOYMENT_KIT_ROOT_DIR can
+# be specified if the GPU Deployment Kit is not installed in a default location.
 #
-# ::
+# FindNVML defines the following variables: 
 #
 #   NVML_INCLUDE_DIR, where to find nvml.h, etc.
-#   NVML_LIBRARIES, the libraries needed to use NVML.
+#   NVML_LIBRARY, the libraries needed to use NVML.
 #   NVML_FOUND, If false, do not try to use NVML.
-
-# GPU_DEPLOYMENT_KIT_ROOT_DIR can be specified if the GPU Deployment Kit is
-# not installed in a default location.
+#
 
 #   Jiri Kraus, NVIDIA Corp (nvidia.com - jkraus)
 #
@@ -46,28 +45,38 @@
 #
 ###############################################################################
 
-set( NVML_LIB_PATHS /usr/lib64 )
-if(GPU_DEPLOYMENT_KIT_ROOT_DIR)
-  set(NVML_LIB_PATHS ${NVML_LIB_PATHS} ${GPU_DEPLOYMENT_KIT_ROOT_DIR}/src/gdk/nvml/lib)
+#TODO: Should GPU_DEPLOYMENT_KIT_ROOT_DIR override the default locations?
+if( CMAKE_SYSTEM_NAME STREQUAL "Windows"  )
+  set( NVML_LIB_PATHS "C:\Program Files\NVIDIA Corporation\GDK" )
+  if(GPU_DEPLOYMENT_KIT_ROOT_DIR)
+    list(APPEND NVML_LIB_PATHS "${GPU_DEPLOYMENT_KIT_ROOT_DIR}\nvml\lib")
+  endif()
+  set(NVML_NAMES nvml)
+  
+  set( NVML_INC_PATHS "C:\Program Files\NVIDIA Corporation\GDK\nvml\include" )
+  if(GPU_DEPLOYMENT_KIT_ROOT_DIR)
+    list(APPEND NVML_INC_PATHS "${GPU_DEPLOYMENT_KIT_ROOT_DIR}\nvml\include")
+  endif()
+else()
+  set( NVML_LIB_PATHS /usr/lib64 )
+  if(GPU_DEPLOYMENT_KIT_ROOT_DIR)
+    list(APPEND NVML_LIB_PATHS "${GPU_DEPLOYMENT_KIT_ROOT_DIR}/src/gdk/nvml/lib")
+  endif()
+  set(NVML_NAMES nvidia-ml)
+  
+  set( NVML_INC_PATHS /usr/include/nvidia/gdk/ /usr/include )
+  if(GPU_DEPLOYMENT_KIT_ROOT_DIR)
+    list(APPEND NVML_INC_PATHS "${GPU_DEPLOYMENT_KIT_ROOT_DIR}/include/nvidia/gdk")
+  endif()
 endif()
 
-set(NVML_NAMES ${NVML_NAMES} nvidia-ml)
-find_library(NVML_LIBRARY NAMES ${NVML_NAMES} HINTS ${NVML_LIB_PATHS} )
+find_library(NVML_LIBRARY NAMES ${NVML_NAMES} PATHS ${NVML_LIB_PATHS} )
 
-set( NVML_INC_PATHS /usr/include/nvidia/gdk/ /usr/include )
-if(GPU_DEPLOYMENT_KIT_ROOT_DIR)
-  set(NVML_INC_PATHS ${NVML_INC_PATHS} ${GPU_DEPLOYMENT_KIT_ROOT_DIR}/include/nvidia/gdk)
-endif()
-
-find_path(NVML_INCLUDE_DIR nvml.h ${NVML_INC_PATHS})
+find_path(NVML_INCLUDE_DIR nvml.h PATHS ${NVML_INC_PATHS})
 
 # handle the QUIETLY and REQUIRED arguments and set NVML_FOUND to TRUE if
 # all listed variables are TRUE
 include(${CMAKE_CURRENT_LIST_DIR}/FindPackageHandleStandardArgs.cmake)
 FIND_PACKAGE_HANDLE_STANDARD_ARGS(NVML DEFAULT_MSG NVML_LIBRARY NVML_INCLUDE_DIR)
 
-if(NVML_FOUND)
-  set(NVML_LIBRARIES ${NVML_LIBRARY})
-endif()
-
-mark_as_advanced(NVML_LIBRARIES NVML_INCLUDE_DIR)
+mark_as_advanced(NVML_LIBRARY NVML_INCLUDE_DIR)
