ec9cbcd293bc5109956108d3f50bb5e488c6a62c
Jiri Kraus <jkraus@nvidia.com>
2015-01-29 07:18:10 -0800

    Some fixes.

--- UnifiedMemory/managed_allocator_test.cu
+++ UnifiedMemory/managed_allocator_test.cu
@@ -2,42 +2,53 @@
 #include <iostream>
 #include "managed_allocator.cuh"
 
+//Use a reference to pass values from arrays in unified memory to a kernel. This avoids a CPU page fault on kernel launch.
 __global__
-void vecadd( double* __restrict__ const c, const double* __restrict__ const a, const double* __restrict__ const b, const int n)
+void vecadd( double& alpha, double* __restrict__ const c, const double* __restrict__ const a, const double* __restrict__ const b, const int n)
 {
 	for (int i = blockIdx.x*blockDim.x + threadIdx.x; i < n; i+=gridDim.x*blockDim.x)
 	{
-		c[i] = a[i] + b[i];
+		c[i] = alpha*a[i] + b[i];
 	}
 }
 
 int main()
 {
-	typedef std::vector<double, managed_allocator<double> > double_vector;
-
 	int n = 1 << 16;
 	
-	double_vector a(n, 0.5);
-	double_vector b(n, 0.5);
-	double* c = new double[n];
-	for ( int i = 0; i < n; ++i )
-		c[i] = 0.0;
-	
-	vecadd<<<64,256>>>(&(c[0]),&(a[0]),&(b[0]),n);
-	cudaDeviceSynchronize();
-	
-	for ( int i = 0; i < n; ++i )
+	typedef std::vector<double, managed_allocator<double> > double_vector;
+
+	cudaSetDevice(0);
+	bool success = true;
 	{
-		if ( abs( c[i] - 1.0 ) > 1E-16 )
+		double_vector alpha(1, 1.0);
+		double_vector a(n, 0.5);
+		double_vector b(n, 0.5);
+		double* c = new double[n];
+		for ( int i = 0; i < n; ++i )
+			c[i] = 0.0;
+	
+		vecadd<<<64,256>>>(alpha[0],&(c[0]),&(a[0]),&(b[0]),n);
+		cudaDeviceSynchronize();
+	
+		for ( int i = 0; i < n; ++i )
 		{
-			std::cerr<<"ERROR: c["<<i<<"] = "<<c[i]<<" != 1.0"<<std::endl;
-			return 1;
+			if ( abs( c[i] - 1.0 ) > 1E-16 )
+			{
+				std::cerr<<"ERROR: c["<<i<<"] = "<<c[i]<<" != 1.0"<<std::endl;
+				success = false;
+				break;
+			}
 		}
+		
+		delete[] c;
 	}
 	
-	std::cout<<"SUCCESS"<<std::endl;
+	if (success)
+	{
+		std::cout<<"SUCCESS"<<std::endl;
+	}
 	
-	delete[] c;
-	
-	return 0;
+	cudaDeviceReset();
+	return success ? 0 : 1;
 }
